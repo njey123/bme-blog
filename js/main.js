19185147c3e828fb37572ddc55ebcb60beb18504
njey123 <njey123@users.noreply.github.com>
2017-06-30 11:22:33 -0400

    Update homepage layout and collapsible nav bar

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
-function main() 
-{
+function main() {
   // Fade in content
-  $('.container-fluid').hide();
-  $('.container-fluid').fadeIn(1000);
+  $('.container-fluid').hide().fadeIn(1000);
+
+  // Slide titles on homepage onto page
+  $('#homepage-titles').hide().fadeIn(200).animate({
+    'margin-top': '3%'
+  }, 400);
+
+  // Hovering over nav bar
+  $( "nav p" ).hover(function() {
+    $(this).toggleClass('nav-p-active');
+  });
 }
 
 $(document).ready(main);
